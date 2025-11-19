1bdee3ab8db87c0fa8f740e416880e0fd6b68e1d
xlebyshek17 <kristina1111113@gmail.com>
2025-11-19 01:41:12 +0100

    initial commit

---
+++ src/main/java/com/kmironenka/eventmasterproject/EventMasterProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kmironenka.eventmasterproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventMasterProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventMasterProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kmironenka/eventmasterproject/EventMasterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmironenka.eventmasterproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventMasterProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
